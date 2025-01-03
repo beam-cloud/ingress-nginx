7d4748c4f676c448f6f392ab7b7d411bec683a6f
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2025-01-03 10:18:37 -0500

    switch image to 1.25

--- images/nginx-1.25/rootfs/build.sh
+++ images/nginx-1.25/rootfs/build.sh
@@ -426,6 +426,9 @@ Include /etc/nginx/owasp-modsecurity-crs/rules/RESPONSE-999-EXCLUSION-RULES-AFTE
 # build nginx
 cd "$BUILD_PATH/nginx-$NGINX_VERSION"
 
+# Clone the ngx_http_proxy_connect_module repository
+git clone https://github.com/chobits/ngx_http_proxy_connect_module.git $BUILD_PATH/ngx_http_proxy_connect_module
+
 # apply nginx patches
 for PATCH in `ls /patches`;do
   echo "Patch: $PATCH"
@@ -435,6 +438,9 @@ for PATCH in `ls /patches`;do
     patch -p1 < /patches/$PATCH
   fi
 done
+
+# Apply patch for ngx_http_proxy_connect_module
+patch -p1 < $BUILD_PATH/ngx_http_proxy_connect_module/patch/proxy_connect_rewrite_102101.patch
 
 WITH_FLAGS="--with-debug \
   --with-compat \
@@ -489,7 +495,8 @@ WITH_MODULES=" \
   --add-dynamic-module=$BUILD_PATH/nginx-http-auth-digest \
   --add-dynamic-module=$BUILD_PATH/ModSecurity-nginx \
   --add-dynamic-module=$BUILD_PATH/ngx_http_geoip2_module \
-  --add-dynamic-module=$BUILD_PATH/ngx_brotli"
+  --add-dynamic-module=$BUILD_PATH/ngx_brotli \
+  --add-dynamic-module=$BUILD_PATH/ngx_http_proxy_connect_module"
 
 ./configure \
   --prefix=/usr/local/nginx \
